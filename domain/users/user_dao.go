12d2b5877cac73d50ad7d535c7058b9c74c2bb06
ninoude <ninoudeaoi@gmail.com>
2020-10-08 12:27:00 +0000

    update

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/ninoude/bookstore_users-api/utils/date_utils"
 	"github.com/ninoude/bookstore_users-api/utils/errors"
+	"github.com/ninoude/bookstore_users-api/datasources/mysql/users_db"
 )
 
 var (
@@ -11,6 +12,10 @@ var (
 )
 
 func (user User) Get() *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
+
 	result := usersDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
